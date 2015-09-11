1c215bffac037d15c27fad5250040834deec8358
codetorex <codetorex@gmail.com>
2015-09-11 23:15:33 +0300

    Added some stuff but will change codebase.

---
+++ src/version.template.h
@@ -0,0 +1,6 @@
+
+#define VERSION_BUILD "{versionbump.build}"
+#define VERSION_MAJOR "{versionbump.major}"
+#define VERSION_MINOR "{versionbump.minor}"
+#define VERSION_SUFFIX "{versionbump.suffix}"
+
